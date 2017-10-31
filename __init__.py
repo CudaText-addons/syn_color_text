1c5a97a48b53f4b9dd65767947d6b7c5bce89486
Хомутов Роман Александрович <R.Khomutov@bssys.com>
2017-10-31 15:19:15 +0300

    +all occurrences in a file

--- __init__.py
+++ __init__.py
@@ -11,17 +11,53 @@ if os.path.isfile(ini0) and not os.path.isfile(ini):
 if app_api_version()<'1.0.145':
     msg_box(MSG_ERROR, 'Plugin needs newer SynWrite')
 
+def set_sel_attribute(n, nlen, attribs):
+    for attrib_set in attribs:
+        att_id, attrib_n = attrib_set.split('|')
+        ed.set_sel(n, nlen)
+        ed.set_attr(int(att_id), int(attrib_n))
+
+def set_text_attribute(attribs):
+    allwords = ini_read(ini, 'setting', 'allwords', '0')
+    if int(allwords) > 0:
+
+        n, nlen = ed.get_sel()
+        if nlen <= 0:
+            carets = ed.get_carets()
+            if carets: return
+            x0, y0 = ed.get_caret_xy()
+
+            n, nlen, word = ed.get_word(x0, y0)
+        else:
+            word = ed.get_text_sel()
+
+        text = ed.get_text_all()
+        ind = 0
+        while ind != -1:
+            if ind >= 0:
+                ind = text.find(word, ind+1)
+            else:
+                ind = text.find(word)
+            if ind >= 0:
+                set_sel_attribute(ind, len(word),attribs)
+        ed.set_sel(n, nlen)
+    else:
+        ed.set_attr(att_id, attrib_n)
+
 def do_color(n):
+    attribs = []
     st = ini_read(ini, 'colors', str(n), '')
     if st:
         ncolor = HTMLColorToPILColor(st)
-        ed.set_attr(ATTRIB_COLOR_BG, ncolor)
+        attribs.extend([str(ATTRIB_COLOR_BG)+'|'+str(ncolor)])
     st = ini_read(ini, 'styles', str(n), '')
     if st:
-        if 'b' in st: ed.set_attr(ATTRIB_SET_BOLD, 0)
-        if 'i' in st: ed.set_attr(ATTRIB_SET_ITALIC, 0)
-        if 'u' in st: ed.set_attr(ATTRIB_SET_UNDERLINE, 0)
-        if 's' in st: ed.set_attr(ATTRIB_SET_STRIKEOUT, 0)
+        if 'b' in st: attribs.extend([str(ATTRIB_SET_BOLD)+'|'+str(0)])
+        if 'i' in st: attribs.extend([str(ATTRIB_SET_ITALIC)+'|'+str(0)])
+        if 'u' in st: attribs.extend([str(ATTRIB_SET_UNDERLINE)+'|'+str(0)])
+        if 's' in st: attribs.extend([str(ATTRIB_SET_STRIKEOUT)+'|'+str(0)])
+    if attribs:
+        set_text_attribute(attribs)
 
 class Command:
     def color1(self): do_color(1)
@@ -30,18 +66,18 @@ class Command:
     def color4(self): do_color(4)
     def color5(self): do_color(5)
     def color6(self): do_color(6)
-        
+
     def format_bold(self):
-        ed.set_attr(ATTRIB_SET_BOLD, 0)
+        set_text_attribute([str(ATTRIB_SET_BOLD)+'|'+str(0)])
     def format_italic(self):
-        ed.set_attr(ATTRIB_SET_ITALIC, 0)
+        set_text_attribute([str(ATTRIB_SET_ITALIC)+'|'+str(0)])
     def format_underline(self):
-        ed.set_attr(ATTRIB_SET_UNDERLINE, 0)
-        
+        set_text_attribute([str(ATTRIB_SET_UNDERLINE)+'|'+str(0)])
+
     def clear_all(self):
         ed.set_attr(ATTRIB_CLEAR_ALL, 0)
     def clear_sel(self):
-        ed.set_attr(ATTRIB_CLEAR_SELECTION, 0)
-        
+        set_text_attribute([str(ATTRIB_CLEAR_SELECTION)+'|'+str(0)])
+
     def edit(self):
         file_open(ini)
